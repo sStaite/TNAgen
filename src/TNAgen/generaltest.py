608a47f7271bcf714e2d6894929e6932a6d0f1bb
sStaite <seamus.staite@gmail.com>
2023-02-09 16:12:29 +1100

    q transform works - not sure if we have any issues

--- src/TNAgen/generaltest.py
+++ src/TNAgen/generaltest.py
@@ -4,15 +4,16 @@ from gwpy.plot import Plot
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn 
 
 def testGenerator(testing):
     generator = Generator()
 
     for test in testing:
-        generator.generate(test, 24, clean=True)
+        generator.generate(test, 96, clean=True)
         #generator.save_as_png("src/data/sanity_images")
 
-    glitch_times = generator.save_as_timeseries(path="src/data/sanity_images", name="test", noise=True, SNR=20, clear_queue=True)
+    glitch_times = generator.save_as_timeseries(path="src/data/sanity_images", name="test", noise=True, SNR=100, clear_queue=True)
     return glitch_times
 
 
@@ -48,8 +49,22 @@ def testRest(testing, glitch_times):
     plt.xlabel('time (s)',fontsize=14)
     plt.ylabel('frequency (Hz)',fontsize=14)
     plt.yscale('log', base=2)
+    plt.colorbar(cmap=spec_cmap, label="Normalized energy")
     plt.savefig("src/data/sanity_images/spectrogram_jade.png")
+    plt.close()
 
+    qspecgram = np.array(white[out[0]: out[1]].q_transform())
+    qspecgram = np.swapaxes(qspecgram, 0, 1)
+    
+    #print(qspecgram)
+    plt.figure(figsize=(8.5, 7))
+    plot = seaborn.heatmap(qspecgram, cmap=spec_cmap)
+    plot.set(ylim=(8, 2048))
+    plot.set_yscale('log', base=2)
+
+    fig = plot.get_figure()
+    fig.savefig("src/data/sanity_images/spectrogram_q.png")
+    plt.close()
 
     """
     plt.plot(strain, 'forestgreen')
@@ -61,7 +76,7 @@ def testRest(testing, glitch_times):
     """
 
 def testSpectrogram():
-    filepath = "src/data/sanity_images/timeseries.hdf5"
+    filepath = "src/data/sanity_images/test.gwf"
 
     strain = TimeSeries.read(filepath)
 
@@ -72,20 +87,15 @@ def testSpectrogram():
 
     window = np.blackman(NFFT)
     spec_cmap='viridis'
-    plt.figure(figsize=(8.5, 7))
-    spec_H1, freqs, bins, im = plt.specgram(strain, NFFT=NFFT, Fs=fs, window=window,
-        noverlap=NOVL, cmap=spec_cmap, scale='linear',mode='magnitude')
-    plt.ylim(10,4096//2)
-    plt.xlabel('time (s)',fontsize=14)
-    plt.ylabel('frequency (Hz)',fontsize=14)
-    plt.savefig("src/data/sanity_images/spectrogram_jade.png")
+    
+    
 
 if __name__ == "__main__":
     all = ["1080Lines", "Extremely_Loud", "Helix", "Light_Modulation",
             "Paired_Doves", "Repeating_Blips", "Scattered_Light", "Scratchy", "Violin_Mode", "Wandering_Line", "Whistle",
             "1400Ripples", "Blip", "Chirp", "Koi_Fish", "Tomte", "Air_Compressor", "Power_Line", "Low_Frequency_Burst", "Low_Frequency_Lines"]
 
-    testing = ["Helix", "Koi_Fish", "Blip", "Chirp"]
+    testing = ["Chirp"]
 
     g = testGenerator(testing)
     testRest(testing, g)
