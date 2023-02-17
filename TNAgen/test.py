a3f8afa9779e25a94311d5480a36fe16eeadeca8
sStaite <seamus.staite@gmail.com>
2023-02-17 11:05:05 +1100

    GPS time added to timeseries and work started on more in depth SNR

--- TNAgen/test.py
+++ TNAgen/test.py
@@ -1,6 +1,4 @@
 import ctypes
+#otsu = ctypes.CDLL("TNAgen/otsu.so")
 
-otsu = ctypes.CDLL("/Users/seamus/Desktop/OzGrav/TNAgen/TNAgen/otsu.so")
-print(otsu.square(2))
-
-# this only works with a homebrew environment! look into this
+#print(otsu.square(2))
