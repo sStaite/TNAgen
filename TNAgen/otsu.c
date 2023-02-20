bcd51ca332c45bc5d13b4c72df45f0894c4456f7
sStaite <seamus.staite@gmail.com>
2023-02-20 16:57:29 +1100

    started on otsu work, ctypes is working:

--- TNAgen/otsu.c
+++ TNAgen/otsu.c
@@ -1,6 +1,18 @@
 
 #include <stdio.h>
 
-int square(int n) {
-    return n*n;
+typedef struct Spectrogram {
+    int     a;
+    float   array[140][170];
+} Spectrogram;
+
+int compute(Spectrogram *img) {
+
+    for (int i = 0; i < 140; i++) {
+        for (int j = 0; j < 170; j++) {
+            printf("%f ", img->array[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
 }
--- TNAgen/test.py
+++ TNAgen/test.py
@@ -1,4 +1,21 @@
-import ctypes
-#otsu = ctypes.CDLL("TNAgen/otsu.so")
+from ctypes import *
+import numpy as np
+# env /usr/bin/arch -x86_64 /bin/zsh --login
+# gcc -shared -o otsu.so -fPIC otsu.c
 
-#print(otsu.square(2))
+data = np.loadtxt("TNAgen/output.txt").tolist()
+
+class Spectrogram(Structure):
+    _fields_ = [("a", c_int),
+                ("array", (c_float * 170) * 140)]
+
+otsu = CDLL("TNAgen/otsu.so")
+otsu.compute.argtypes = [POINTER(Spectrogram)]
+
+t = Spectrogram()
+
+for i in range(140):
+    for j in range(170):
+        t.array[i][j] = data[i][j]
+
+otsu.compute(byref(t))
